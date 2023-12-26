45bcd1c9045f0a595ccf95f3fc5ea39e1ed55342
ryosyntaxx <riodwi12174@gmail.com>
2023-12-26 17:57:24 +0700

    add function request PUT

--- APIRetry/ApiRetrys.py
+++ APIRetry/ApiRetrys.py
@@ -176,7 +176,7 @@ class ApiRetry:
                     logger.info(f"retry to: {retry}")
 
                 elif self.show_logs and response.status_code != 200:
-                    logger.warning(f"method: head")
+                    logger.warning(f"method: HEAD")
                     logger.warning(f"status code: {response.status_code}")
                     logger.warning(f"retry to: {retry}")
 
@@ -191,3 +191,62 @@ class ApiRetry:
         
         raise MaxRetryExceptions(message=f"Failed to retrieve data after {max_retries} retries. The server may be unreachable or experiencing issues")
 
+    def put(
+            self, 
+            url: str, 
+            headers: dict, 
+            max_retries: int = 5, 
+            retry_interval: Union[int, float] = 0.2,
+            data    = None,
+            params  = None,
+            cookies = None,
+            files   = None,
+            auth    = None,
+            timeout = None,
+            proxies = None,
+            hooks   = None,
+            stream  = None,
+            verify  = None,
+            cert    = None,
+            json    = None,
+            ) -> Response:
+        
+        for retry in range(max_retries):
+            try:
+                response    = requests.put(
+                    url     = url,
+                    data    = data,
+                    params  = params,
+                    headers = headers,
+                    cookies = cookies,
+                    files   = files,
+                    auth    = auth,
+                    timeout = timeout,
+                    proxies = proxies,
+                    hooks   = hooks,
+                    stream  = stream,
+                    verify  = verify,
+                    cert    = cert,
+                    json    = json,
+                )
+
+                if self.show_logs and response.status_code == 200:
+                    logger.info(f"method: PUT")
+                    logger.info(f"status code: {response.status_code}")
+                    logger.info(f"retry to: {retry}")
+
+                elif self.show_logs and response.status_code != 200:
+                    logger.warning(f"method: PUT")
+                    logger.warning(f"status code: {response.status_code}")
+                    logger.warning(f"retry to: {retry}")
+
+
+                return response
+            except Exception as err:
+                logger.error(f'message: {err}')
+                logger.warning(f'try the request again')
+                
+            sleep(retry_interval)
+            retry_interval+= 0.2
+        
+        raise MaxRetryExceptions(message=f"Failed to retrieve data after {max_retries} retries. The server may be unreachable or experiencing issues")
